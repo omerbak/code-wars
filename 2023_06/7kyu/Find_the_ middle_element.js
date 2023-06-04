5980c76e12aba3f3811cff75f47a70f2d46c030e
omerbak <42501059+omerbak@users.noreply.github.com>
2023-06-04 13:31:53 +0300

    solved find the middle element problem 7kyu

--- 2023_06/7kyu/Find_the_ middle_element.js
+++ 2023_06/7kyu/Find_the_ middle_element.js
@@ -14,19 +14,32 @@ gimme([5, 10, 14]) => 1
 
 /* 
 parameters:
-
+-are we always going to get a triplet ?  
+meanig no empty array or an array with more or less than three elements
+- are the elements of the triplet always numbers?
 */
 
 /* 
 return:
-
+    the index (number) of the middle element of the triplet in value;
 */
 
 /* 
 Examples:
-
+[5, 8, 10]   ====> 1
+[4, 9, 2]   ====> 0;
 */
 /* 
 psudo code:
-
+    1- make a copy of the triplet entered in parameter and sort it from lower to higher
+    2- after sorting the middle number "our target" is in index 1
+    3- return the the index of target in the triplet entered in parameter using the method indexOf   
 */
+
+function gimme(triplet) {
+  let sortedTriplet = triplet.slice(0).sort((a, b) => a - b);
+  let targetNumber = sortedTriplet[1];
+  return triplet.indexOf(targetNumber);
+}
+console.log(gimme([5, 8, 10]));
+console.log(gimme([4, 9, 2]));

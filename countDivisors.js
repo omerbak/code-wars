51953ac42d224c0883b04d8ce7bf02910c47043c
omerbak <42501059+omerbak@users.noreply.github.com>
2023-06-16 16:40:43 +0300

    modify divisors count algo

--- countDivisors.js
+++ countDivisors.js
@@ -1,9 +1,11 @@
 function getDivisorsCnt(n) {
-  let divisorsArr = [];
-  for (let i = n; i > 0; i--) {
+  if (n <= 0) return 0;
+  let divsorsNum = 0;
+  if (n % Math.sqrt(n) === 0) divsorsNum++;
+  for (let i = 1; i < Math.sqrt(n); i++) {
     if (n % i === 0) {
-      divisorsArr.push(i);
+      divsorsNum += 2;
     }
   }
-  return divisorsArr.length;
+  return divsorsNum;
 }

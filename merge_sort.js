6a8cb44674a14a0d51a92064f7d3d6a3476d5328
omerbak <42501059+omerbak@users.noreply.github.com>
2023-10-30 19:33:44 +0300

    merge_sort.js

---
+++ merge_sort.js
@@ -0,0 +1,24 @@
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(leftArr, rightArr) {
+  let sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] < rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+/* console.log(merge([1, 3], [2, 5])); */
+console.log(mergeSort([1, 3, 2, 5, -1, 90, -30]));
